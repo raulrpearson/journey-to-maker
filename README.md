f71ea842c8a162367f62fce8f86960c4eaa41d5c
Raúl R. Pearson <raulrpearson@protonmail.com>
2019-02-02 12:51:31 +0700

    Initialize basic Gatsby MDX setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Journey To Maker
+
+A journal, wiki, TODO registry of my effort to become a successful
+indiehacker/maker entrepreneur.
---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pathPrefix: '/journey-to-maker',
+  siteMetadata: {
+    title: 'Journey To Maker',
+    description:
+      'A journal, wiki, TODO registry of my effort to become a successful indiehacker/maker entrepreneur.',
+    author: '@raulrpearson'
+  },
+  plugins: ['gatsby-mdx', 'gatsby-plugin-catch-links']
+};
